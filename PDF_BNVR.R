e5ed6c718b74b4598c32ed0bf52a64193eec22f1
lukas.beinhauer <lukas.beinhauer@gmail.com>
2022-04-09 16:35:22 +0200

    Update PDF_BNVR.R
    added function combining all pdf-functions

--- PDF_BNVR.R
+++ PDF_BNVR.R
@@ -65,7 +65,6 @@ p_cor_cent <- function(z, sig_y, sig_x, cor){
 
 
 
-
 # correlated non-central normal ratio (Katz Approx.)
 
 z = (x + mu_x)/(y + mu_y)
@@ -87,6 +86,66 @@ z = (xadj + (cor*y*(sig_x/sig_y)) + mu_x)/(y + mu_y)
 
 
 
+
+
+pdfBNVR <- function(z, sig_y = 1, sig_x = 1, mu_x = 0, mu_y = 0, cor = 0){
+  
+  # uncorrelated central
+  if(cor == 0 & sum((c(mu_x, mu_y) == 0)) == 2){
+    p <- p_uncor_cent(z)
+    info <- "uncorrelated central"
+  }
+  
+  # uncorrelated non-central
+  if(cor == 0 & sum((c(mu_x, mu_y) == 0)) != 2){
+    p <- p_uncor(z, sig_y, sig_x, mu_x, mu_y)
+    info <- "uncorrelated non-central"
+  }
+  
+  # correlated central
+  if(cor != 0 & sum((c(mu_x, mu_y) == 0)) == 2){
+    p <- p_cor_cent(z, sig_y, sig_x, cor)
+    info <- "correlated central"
+  }
+  
+  # correlated non-central
+  if(cor != 0 & sum((c(mu_x, mu_y) == 0)) != 2){
+    stop("PDF for Ratio of correlated, non-central bivariate normal distribution currently not implemented!")
+  }
+  
+  c(p, info)
+  
+}
+
+mat <- matrix(NA, nrow = length(seq(-3, 3, .1)), ncol = 2)
+
+for(i in 1:length(seq(-3, 3, .1))){
+  z <- seq(-3, 3, .1)[i]
+  mat[i,1] <- pdfBNVR(z = z)[1]
+  mat[i,2] <- z
+}
+
+plot(mat[,2], mat[,1])
+
+
+for(i in 1:length(seq(-3, 3, .1))){
+  z <- seq(-3, 3, .1)[i]
+  mat[i,1] <- pdfBNVR(z = z, mu_x = 10, mu_y = 10, sig_x = 3, sig_y = 3)[1]
+  mat[i,2] <- z
+}
+
+plot(mat[,2], mat[,1])
+
+
+
+for(i in 1:length(seq(-3, 3, .1))){
+  z <- seq(-3, 3, .1)[i]
+  mat[i,1] <- pdfBNVR(z = z, mu_x = 0, mu_y = 0, sig_x = 1, sig_y = 1, cor = .5)[1]
+  mat[i,2] <- z
+}
+
+
+pdfBNVR(z = 0, mu_x = 1, mu_y = 0, sig_x = 1, sig_y = 1, cor = .5)
 
 
 
